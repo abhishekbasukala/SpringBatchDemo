667e95a813ff9881a68baad2fc779519aaa01bb9
abhishekbasukala <abhishekbasukala@gmail.com>
2017-01-23 18:17:53 -0500

    initail commit

---
+++ src/main/java/com/springbatch/CustomItemProcessor.java
@@ -0,0 +1,16 @@
+package com.springbatch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.springbatch.model.Report;
+
+public class CustomItemProcessor implements ItemProcessor<Report, Report> {
+
+	@Override
+	public Report process(Report item) throws Exception {
+
+		System.out.println("Processing..." + item);
+		return item;
+	}
+
+}
